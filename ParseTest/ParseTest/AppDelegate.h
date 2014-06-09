216095bac9465a1969a9639af08855ec4ecef80f
francisco-dechoudens <francisco.dechoudens@gmail.com>
2014-06-09 14:47:51 -0400

    add a Parse testing project.

---
+++ ParseTest/ParseTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ParseTest
+//
+//  Created by Frankie on 6/9/14.
+//  Copyright (c) 2014 Francisco L. De Choudens Ortiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ParseTest/ParseTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ParseTest
+//
+//  Created by Frankie on 6/9/14.
+//  Copyright (c) 2014 Francisco L. De Choudens Ortiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
