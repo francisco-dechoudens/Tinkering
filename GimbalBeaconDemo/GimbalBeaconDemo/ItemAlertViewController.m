32dc965f654c217f8cd45164e51f23ca2ed9bd1e
francisco-dechoudens <francisco.dechoudens@gmail.com>
2014-10-21 10:23:41 -0400

    Add AudioControl and Autoplay

--- GimbalBeaconDemo/GimbalBeaconDemo/ItemAlertViewController.m
+++ GimbalBeaconDemo/GimbalBeaconDemo/ItemAlertViewController.m
@@ -10,15 +10,20 @@
 #import "AppDelegate.h"
 #import <AVFoundation/AVFoundation.h>
 
-@interface ItemAlertViewController () <UITableViewDataSource,UITableViewDelegate>
+@interface ItemAlertViewController () <UITableViewDataSource,UITableViewDelegate, AVAudioPlayerDelegate>
 @property (weak, nonatomic) IBOutlet UIView *backgroundView;
 @property (weak, nonatomic) IBOutlet UIView *secondView;
 @property (weak, nonatomic) IBOutlet UITableView *playListTableView;
 @property (strong, nonatomic) AVAudioPlayer* audioPlayer;
+@property (nonatomic, strong) NSTimer* timer;
+@property (weak, nonatomic) IBOutlet UISlider *currentTimeSlider;
+@property (weak, nonatomic) IBOutlet UILabel *elapsedTimeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *remainingTimeLabel;
 @end
 
 @implementation ItemAlertViewController{
     int counter;
+    int currentPlayedAudio;
     NSArray* tempAudioArray;
 }
 
@@ -38,10 +43,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    currentPlayedAudio = 0;
     counter = 1;
     
     
-    tempAudioArray = @[@"2 Zoo Intro",@"6 Chimpance"];
+    tempAudioArray = @[@"2 Zoo Intro",@"6 Chimpance",@"8 Sapo Concho",@"15 Culebra Maicera"];
     
     
     self.playListTableView.delegate = self;
@@ -55,6 +61,7 @@
     self.secondView.layer.borderWidth = 2.0;
     self.secondView.layer.borderColor = [UIColor purpleColor].CGColor;
     
+    [self addingAudioForIndex:currentPlayedAudio];//autoplay first video
 }
 
 
@@ -102,7 +109,7 @@
 
 - (void)addToPlayListTable{
     counter++;
-    [self incrementTableView];
+    [self incrementTableViewWithIndex:[NSIndexPath indexPathForRow:counter-1 inSection:0]];
 }
 
 - (void)rmvFromPlayListTableTransmitter:(Transmitter*)transmitter{
@@ -117,20 +124,126 @@
 }
 
 -(void)addingAudioForIndex:(NSUInteger)index{
+    
+    //Higlight first cell
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:index inSection:0];
+    [self.playListTableView selectRowAtIndexPath:indexPath animated:NO scrollPosition:UITableViewScrollPositionNone];
    
     NSError *error;
     
     NSString *path = [[NSBundle mainBundle] pathForResource:tempAudioArray[index] ofType:@"mp3"];
 
     self.audioPlayer = [[AVAudioPlayer alloc]initWithContentsOfURL:[NSURL URLWithString:[path stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] error:&error];
+    self.audioPlayer.delegate = self;
+
+    self.currentTimeSlider.minimumValue = 0.0f;
+    self.currentTimeSlider.maximumValue = self.audioPlayer.duration;
+    currentPlayedAudio = index;
+    [self updateDisplay];
+    [self play:self];
+}
+
+#pragma mark - Actions
+
+- (IBAction)play:(id)sender {
+    //IDZTrace();
     [self.audioPlayer prepareToPlay];
     [self.audioPlayer play];
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(timerFired:) userInfo:nil repeats:YES];
+}
+
+- (IBAction)pause:(id)sender {
+   // IDZTrace();
+    [self.audioPlayer pause];
+    [self stopTimer];
+    [self updateDisplay];
+}
+
+- (IBAction)stop:(id)sender {
+  //  IDZTrace();
+    [self.audioPlayer stop];
+    [self stopTimer];
+    self.audioPlayer.currentTime = 0;
+    [self.audioPlayer prepareToPlay];
+    [self updateDisplay];
+}
+
+- (IBAction)currentTimeSliderValueChanged:(id)sender
+{
+    if(self.timer)
+        [self stopTimer];
+    [self updateSliderLabels];
+}
+
+- (IBAction)currentTimeSliderTouchUpInside:(id)sender
+{
+    [self.audioPlayer stop];
+    self.audioPlayer.currentTime = self.currentTimeSlider.value;
+    [self.audioPlayer prepareToPlay];
+    [self play:self];
+}
+
+#pragma mark - Timer
+- (void)timerFired:(NSTimer*)timer
+{
+    [self updateDisplay];
+}
+
+- (void)stopTimer
+{
+    [self.timer invalidate];
+    self.timer = nil;
+    [self updateDisplay];
+}
+
+#pragma mark - Display Update
+- (void)updateDisplay
+{
+    NSTimeInterval currentTime = self.audioPlayer.currentTime;
+    self.currentTimeSlider.value = currentTime;
+    [self updateSliderLabels];
+}
+
+- (void)updateSliderLabels
+{
+    NSTimeInterval currentTime = self.currentTimeSlider.value;
+    NSString* currentTimeString = [NSString stringWithFormat:@"%.02f", currentTime];
+    
+    self.elapsedTimeLabel.text =  currentTimeString;
+    self.remainingTimeLabel.text = [NSString stringWithFormat:@"%.02f", self.audioPlayer.duration - currentTime];
+}
+
+#pragma mark - AVAudioPlayerDelegate
+- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag
+{
+    NSLog(@"%s successfully=%@", __PRETTY_FUNCTION__, flag ? @"YES"  : @"NO");
+    [self stopTimer];
+    [self updateDisplay];
+    
+    if (currentPlayedAudio < counter-1) {
+        [self addingAudioForIndex:++currentPlayedAudio];//Play next audio
+    }
+    else{
+        currentPlayedAudio = 0; //start over
+    }
+    
+   
+}
+
+- (void)audioPlayerDecodeErrorDidOccur:(AVAudioPlayer *)player error:(NSError *)error
+{
+    NSLog(@"%s error=%@", __PRETTY_FUNCTION__, error);
+    [self stopTimer];
+    [self updateDisplay];
 }
 
 #pragma mark - animation Method
 
--(void)incrementTableView{
-    [self.playListTableView reloadData];
+-(void)incrementTableViewWithIndex:(NSIndexPath*)indexToAdd{
+    
+    [self.playListTableView  beginUpdates];
+    [self.playListTableView insertRowsAtIndexPaths:@[indexToAdd] withRowAnimation:UITableViewRowAnimationNone];
+    [self.playListTableView  endUpdates];
     
     if (self.transmitters.count < 5) {
         [UIView animateWithDuration:0.5
